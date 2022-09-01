1ee02abb324684d381a419da0eb1ef07f7f43739
Jederson Gabriel <89799559+Jeta8@users.noreply.github.com>
2022-09-01 11:39:16 -0300

    Create verify.cs

---
+++ Update/verify.cs
@@ -0,0 +1,20 @@
+private void f_main_Load(object sender, EventArgs e)
+    {
+        VerifyUpdate();
+    }
+
+private void VerifyUpdate()
+        var vlocal = new Version(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+    
+        var vremot = new Version(DownloadVersion().FirstOrDefault());
+
+
+        var result = vlocal.CompareTo(vremot);
+        if (result < 0)
+        {
+            UpdateQuestion();
+        }
+        else
+        {
+            AlreadyUpdateQuestion();
+        }
